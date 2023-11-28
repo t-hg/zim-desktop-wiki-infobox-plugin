b5efd92703afc5273dcc6f6adec98e0d30f18435
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-11-29 00:04:44 +0100

    refactor get style

--- InfoBox.py
+++ InfoBox.py
@@ -46,19 +46,29 @@ class InfoBoxBuffer(Gtk.TextBuffer):
 		return self.attrib, text
 
 
+def get_style(key, default, _type):
+	try:
+		style = ConfigManager.get_config_dict('style.conf')
+		value = style['Tag info'][key]
+		if _type == 'string':
+			return value
+		elif _type == 'color':
+			color_ok, color = Gdk.Color.parse(value)
+			if color_ok:
+				return color
+			else:
+				return default
+		else:
+			return default
+	except KeyError:
+		return default
+
+
 class InfoBoxWidget(TextViewWidget):
 	def _init_view(self):
 		TextViewWidget._init_view(self)
-		font = self._get_style('family', 'monospace')
+		font = get_style('family', 'monospace', 'string')
+		background = get_style('background', 'blue', 'color')
 		self.view.modify_font(Pango.FontDescription(font))
-		background_ok, background = Gdk.Color.parse(self._get_style('background', 'blue'))
-		if background_ok:
-			self.view.modify_bg(Gtk.StateType.NORMAL, background)
-
-	def _get_style(self, key, default):
-		try:
-			style = ConfigManager.get_config_dict('style.conf')
-			return style['Tag info'][key]
-		except KeyError:
-			return default
+		self.view.modify_bg(Gtk.StateType.NORMAL, background)
 
